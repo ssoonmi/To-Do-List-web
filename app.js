2eee43319ffdb1269232e9dcb7db25e86906bc03
Soon-Mi <ssoonmi@gmail.com>
2019-04-16 19:16:02 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.sendFile('index.html', { root: __dirname }))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+app.use('/public', express.static(__dirname + '/public'));
---
+++ frontend/entry.jsx
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+document.addEventListener('DOMContentLoaded', function() {
+    ReactDOM.render(
+        <h1>Hello World!</h1>, 
+        document.getElementById('root')
+    );
+});
